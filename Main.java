eec450c668ac699c98bf53b891b2d564669a351d
Deepak <dvcool21@gmail.com>
2023-12-07 00:10:44 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Backend;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        createAccount acc1 = new createAccount("deepak","varshney","8394906780","pass");
+//        System.out.println(validateUser.validate_user("431936805","pasws"));
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
